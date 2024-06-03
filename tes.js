c0839e2fdd4184b2104f476bc45fbdc6941d2300
Rakha <rakhaaz@yahoo.com>
2024-06-03 20:41:17 +0700

    menambah file HTML

--- tes.js
+++
@@ -1,5 +0,0 @@
-// Based on the array below write a function that will return primitive data types only. let
-// arr = [1, [], undefined, {}, "string", {}, []];
-// a. The function will return [1, undefined, “string”]
-
-console.log(Math.random());
