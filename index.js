1cb28bdd17d7b80ad2f75a9790b7f7fa1ed5866f
rakha <rakhaaz@yahoo.com>
2024-06-13 13:36:51 +0700

    edit file

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var x = prompt("1. Input" + "\n" + "2. Show")
+let arr = []
 
-if (x == 1){
-
-class StudentRegistration{
+class stdPurwa {
     name;
     username;
     email;
@@ -10,115 +8,64 @@ class StudentRegistration{
     address;
     #phoneNumber;
     programSelected;
-
-    constructor(name, username, address, phoneNumber){
-        this.name = name;
-        this.username = username; 
-        this.address = address;
-        this.#phoneNumber = phoneNumber; 
-    }
-
-    
-     set validateName(name){
+    constructor (name, username, address, phoneNumber ){
         this.name = name
-    }
+        this.username = username
+        this.address = address
+        this.#phoneNumber = phoneNumber
+       }
+
     
-
-    set validateEmail(email){
-        if(email.includes('@') && email.includes('.')){
+    
+    set validateEmail(email){       //memvalidasi email
+        if(!email.includes('@')){
+            console.log('Email Not Valid!')
+        }else{
             this.email = email
-        }else{
-            console.log('Email Not Valid');
         }
     }
-
-
-    set validatePassword(password){
-        if(password.length < 6 || password.length > 10){
-            console.log('Password Have Minimum Require')
-        }else{
-            this.#password = password
+    set validatePassword (password){
+        if (password.length<6 || password.length>10){
+            console.log('Password Not Valid!');
         }
+        else {this.#password = password}
     }
 
-   
-    set validateProgramSelected(program){
-        const validPrograms = ['JCWD', 'JCDS', 'JCUI/UX', 'JCDM', 'JCVD']
-
-        if(validPrograms.includes(program.toUpperCase())){
-            this.programSelected = program.toUpperCase()
-        }else{
-            console.log('Program Not Found!')
-        }
+    set uppercase(programSelected){
+        const pwd = ['JCWDOL', 'JCDSOL', 'JCUI/UX', 'JCDM', 'JCVD']
+        programSelected = programSelected.toUpperCase()
+        if (pwd.includes(programSelected)){
+        return this.programSelected = programSelected}
     }
 
     get showPhoneNumber(){
-        console.log(this.#phoneNumber.slice(0, this.#phoneNumber.length-6) + '*****')
-        }
-    }
+         console.log(this.#phoneNumber.slice(0, this.#phoneNumber.length-6) + '*****')
+         }
 
 
-
-    const student1 = new StudentRegistration(prompt ('Masukkan nama'), prompt ('Masukkan username'), prompt ('Masukkan lokasi'), prompt ('Masukkan telepon'))
-    
-    student1.validateEmail = 'rakhaaz@yahoo.com'
-    student1.validatePassword = 'abc12345'
-    student1.validateProgramSelected = 'jcwd'
-    student1.showPhoneNumber
-    console.log(student1)
 }
 
+let input= Number(prompt('1. Insert\n2. Show')) 
+while (input == 1){
+    
+
+    let nama = prompt('Masukkan nama')
+    let userName1 = prompt('Masukkan username')
+    let daerah = prompt('Masukkan daerah')
+    let noTlp = prompt('Masukkan no. HP')
 
 
+    const user1 = new stdPurwa (nama, userName1, daerah, noTlp)
+    arr.push(user1)
+    console.log(arr);
+    input= Number(prompt('1. Insert\n2. Show'))
+} 
 
-// class StudentRegistration{
-//     name;
-//     username;
-//     email;
-//     #password;
-//     address;
-//     #phoneNumber;
-//     programSelected;
 
-//     constructor(name, username, address, phoneNumber){
-//         this.name = name;
-//         this.username = username; 
-//         this.address = address;
-//         this.#phoneNumber = phoneNumber; 
-//     }
+    alert (arr) 
 
-//     set validateEmail(email){
-//         if(email.includes('@') && email.includes('.')){
-//             this.email = email
-//         }else{
-//             console.log('Email Not Valid');
-//         }
-//     }
-//     set validatePassword(password){
-//         if(password.length < 6 || password.length > 10){
-//             console.log('Password Have Minimum Require')
-//         }else{
-//             this.#password = password
-//         }
-//     }
-//     set validateProgramSelected(program){
-//         const validPrograms = ['JCWD', 'JCDS', 'JCUI/UX', 'JCDM', 'JCVD']
-
-//         if(validPrograms.includes(program.toUpperCase())){
-//             this.programSelected = program.toUpperCase()
-//         }else{
-//             console.log('Program Not Found!')
-//         }
-//     }
-
-//     get showPhoneNumber(){
-//         console.log(this.#phoneNumber.slice(0, this.#phoneNumber.length-6) + '*****')
-//     }
-// }
-
-// const student1 = new StudentRegistration('Ryan', 'ryan01', 'Bogor', '0888')
-// student1.validateEmail = 'ryan@gmail.com'
-// student1.validatePassword = 'abc12345'
-// student1.validateProgramSelected = 'jcwd'
-// student1.showPhoneNumber
-// console.log(student1)
+// user1.validateEmail = 'rakhaaz@yahoo.com'
+// user1.validatePassword = 'abcdefghijklmn'
+// user1.uppercase = 'jCWdol'
+// user1.showPhoneNumber
+// console.log(user1);
